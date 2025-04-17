882d0551081e96d0bd8cc3d184c274a447f58943
Martian <marvs298@yahoo.com>
2025-04-17 11:38:13 -0400

    more CSS changes for the BetsList

--- src/components/BetsList.jsx
+++ src/components/BetsList.jsx
@@ -5,7 +5,7 @@ import images from '../util/IMAGES';
 export default function BetsList() {
     const { bets } = useLoaderData();
     const data = useRouteLoaderData("bets-root");
-    console.log(images["ongoing"]);
+
     return (
         <>
             <div className={classes.bets}>
@@ -15,47 +15,51 @@ export default function BetsList() {
                         <li key={bet.betId} className={classes.item}>
                             <Link to={`/bets/${bet.betId}`} className={`${bet.status === 'complete' ? classes.complete : ''}`}>
                                 <div className={classes["bet-container"]}>
-                                    <img src={`${images[bet.status.trim().length > 0 ? bet.status : 'ongoing'].link}.jpg`} alt={bet.title} />
-                                    <div className={classes.content}>
-                                        <span>{bet.title}</span>
-                                        <br />
-                                        <div className={classes.leparticipants}>
-                                            {(bet.players.teamA.map((player) => {
-                                                const aPlayer = data.find((p) => p.id === player);
-                                                return aPlayer.name
-                                            })).join(",")}
-                                            &nbsp;VS&nbsp;
-                                            {(bet.players.teamB.map((player) => {
-                                                const aPlayer = data.find((p) => p.id === player);
-                                                return aPlayer.name
-                                            })).join(",")}
-                                        </div>
+                                    <div className={classes["left-bets-image"]}>
+                                        <img src={`${images[bet.status.trim().length > 0 ? bet.status : 'ongoing'].link}.jpg`} alt={bet.title} />
                                     </div>
-                                    {bet.status.trim().length > 0 ? (
-                                        <div>
-                                            <p>
-                                                Status: <span className={classes.status}>{bet.status}</span>
-                                            </p>
-                                            {
-                                                bet.winner.trim().length > 0 ? (
-                                                    <p>
-                                                        Winner Team: {
-                                                            bet.winner === "teamA" ?
-                                                                bet.players.teamA.map((player) => {
-                                                                    const aPlayer = data.find((p) => p.id === player);
-                                                                    return aPlayer.name
-                                                                })
-                                                                :
-                                                                bet.players.teamB.map((player) => {
-                                                                    const aPlayer = data.find((p) => p.id === player);
-                                                                    return aPlayer.name
-                                                                })
-                                                        }
-                                                    </p>
-                                                ) : ''
-                                            }
+                                    <div>
+                                        <div className={classes.content}>
+                                            <span>{bet.title}</span>
+                                            <br />
+                                            <div className={classes.leparticipants}>
+                                                {(bet.players.teamA.map((player) => {
+                                                    const aPlayer = data.find((p) => p.id === player);
+                                                    return aPlayer.name
+                                                })).join(",")}
+                                                &nbsp;VS&nbsp;
+                                                {(bet.players.teamB.map((player) => {
+                                                    const aPlayer = data.find((p) => p.id === player);
+                                                    return aPlayer.name
+                                                })).join(",")}
+                                            </div>
                                         </div>
-                                    ) : ''}
+                                        {bet.status.trim().length > 0 ? (
+                                            <div>
+                                                <p>
+                                                    Status: <span className={classes.status}>{bet.status}</span>
+                                                </p>
+                                                {
+                                                    bet.winner.trim().length > 0 ? (
+                                                        <p>
+                                                            Winner Team: {
+                                                                bet.winner === "teamA" ?
+                                                                    bet.players.teamA.map((player) => {
+                                                                        const aPlayer = data.find((p) => p.id === player);
+                                                                        return aPlayer.name
+                                                                    })
+                                                                    :
+                                                                    bet.players.teamB.map((player) => {
+                                                                        const aPlayer = data.find((p) => p.id === player);
+                                                                        return aPlayer.name
+                                                                    })
+                                                            }
+                                                        </p>
+                                                    ) : ''
+                                                }
+                                            </div>
+                                        ) : ''}
+                                    </div>
                                 </div>
                             </Link>
                         </li>))}
